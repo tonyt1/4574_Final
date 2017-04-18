499fd57d8f684e263146eb844c4ec807217b624e
Ubuntu <ubuntu@ip-172-31-1-6.us-west-2.compute.internal>
2017-04-18 03:56:37 +0000

    Updated server for Authentication. Links to AWS server

--- AuthServer/Auth/web/swagger_server/controllers/accounts_controller.py
+++ AuthServer/Auth/web/swagger_server/controllers/accounts_controller.py
@@ -5,9 +5,14 @@ from six import iteritems
 from ..util import deserialize_date, deserialize_datetime
 import json
 from flask import jsonify
-from flask.ext.api import status
+from flask_api import status
 from pymongo import MongoClient
+from flask import request
+import os
 
+client = MongoClient(os.environ.get("DB_PORT_27017_TCP_ADDR"),27017)
+pass_db = client.passdb
+pass_posts = pass_db.posts
 
 def create_account():
     """
@@ -16,7 +21,20 @@ def create_account():
 
     :rtype: None
     """
-    return 'do some magic!'
+    auth = request.authorization
+    if auth is not None:
+        found_username = pass_posts.find_one({"username": auth.username})
+        if not found_username:
+
+            credentials = {"username": auth.username,
+                           "password": auth.password}
+            pass_posts.insert_one(credentials)
+            return Response("Created account!", 200)
+        else:
+            return Response("Username already exists", 409)
+    else:
+        return Response("Authentication requried", 400)
+    print(auth)
 
 
 def login_account():
@@ -26,4 +44,23 @@ def login_account():
 
     :rtype: None
     """
-    return 'do some magic!'
+    print("Getting authorization headers")
+    auth = request.authorization
+    if auth is not None:
+        print("Finding User ", username)
+        found_user = pass_posts.find_one({"username": username})
+        if found_user is not None:
+            print("Found User")
+            if password == found_user["password"]:
+                print("Successful Authentication")
+                return Response("Successful Authentication",200)
+            else:
+                print("Failed Authentication (Password)")
+                return Response("Invalid credentials", 401)
+        else:
+            print("Failed Authentication (Username")
+            return Response("Invalid credentials", 401)   
+    else:
+        return Response("Authentication requried", 400)
+        
+
