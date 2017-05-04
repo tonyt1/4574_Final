66a6b318c3fcc5b5450aba59784ad3057a4fc73f
Jonathan <jondef95@vt.edu>
2017-05-04 02:03:18 -0400

    changes to factauth docker stuff

--- Fact_Auth/README.md
+++ Fact_Auth/README.md
@@ -1,6 +1,6 @@
 # Authentication and Fact Generator Server
 
-This contribution to the team project contains the Python-Flask server that serves the API for fact generation and authentication. The front-end for facts contains only two entrypoints, both GET commands. One command will return a random fact, and another will attempt to find a fact about the topic that is input by the user. The front-end for authentication contains two endpoints, a GET and a POST. The GET is used to login and retrive a token for an existing account. The POST is used to create a new account on the server database.
+This contribution to the team project contains the Python-Flask server that serves the API for fact generation and authentication. The front-end for facts contains only two entrypoints, both GET commands. One command will return a random fact, and another will attempt to find a fact about the topic that is input by the user. The front-end for authentication contains two endpoints, a GET and a POST. The GET is used to login and retrieve a token for an existing account. The POST is used to create a new account on the server database.
 
 ## Running the Code
 There are two Docker images currently available to run:
@@ -10,18 +10,8 @@ To run an image without authentication, either download the [Docker Image](https
 
 `sudo docker run -p 8080:8080 jondef95/fact-gen-server`
 
-or use the Dockerfile in the api-server directory:
-
-`sudo docker build -t fact-gen-server api-server/`
-
-`sudo docker run -p 8080:8080 fact-gen-server`
-
 ### With Authentication
-To run an image of the fact generator with authentication, either download the [Docker Image](https://hub.docker.com/r/jondef95/factauth_web) by running the commands:
-
-`sudo docker run -p 8000:8000 jondef95/factauth_web`
-
-or use the Docker-Compose file in the Fact_Auth directory:
+To run an image of the fact generator with authentication, use the Docker-Compose file in the Fact_Auth directory:
 
 `sudo docker-compose up Fact_Auth/`
 
--- Fact_Auth/testing/api_testing.py
+++ Fact_Auth/testing/api_testing.py
@@ -1,7 +1,11 @@
-import unittest, mock, requests
+import unittest, mock, requests, time, sys
 import fact_generator as FG
+from time import sleep
 
-api_url = "http://127.0.0.1:8080/api"
+api_url = "http://127.0.0.1:8000/facts"
+username = ""
+password = ""
+token = ""
 
 class TestInternalMethods(unittest.TestCase):
 
@@ -16,6 +20,7 @@ class TestInternalMethods(unittest.TestCase):
 		self.assertEqual(split_sentences[1], "It should split now.")
 		self.assertEqual(split_sentences[2], "This is the end.")
 		print("\n\n")
+		sleep(3)
 
 
 	def test_get_fact_subject(self):
@@ -28,7 +33,8 @@ class TestInternalMethods(unittest.TestCase):
 		self.assertEqual(return_subject, 'Barack Obama')
 		self.assertTrue('Obama' in facts)
 		print(response)
-		print("\n")
+		print("\n\n")
+		sleep(3)
 
 
 	def test_get_fact_subject_fail(self):
@@ -41,7 +47,8 @@ class TestInternalMethods(unittest.TestCase):
 		self.assertEqual(return_subject, 'error')
 		self.assertTrue('ergopoiewgporewom' in facts)
 		print(response)
-		print("\n")
+		print("\n\n")
+		sleep(3)
 
 
 	def test_get_fact_random(self):
@@ -56,51 +63,76 @@ class TestInternalMethods(unittest.TestCase):
 		print(subject1, "  -----  ", subject2)
 		self.assertFalse(subject1 == subject2)
 
-		print("\n")
+		print("\n\n")
+		sleep(3)
 
 class TestExternalMethods(unittest.TestCase):
 
 	@unittest.skipIf(api_url is "", "This test is disabled until a URL has been defined for the API")
 	def test_get_fact_subject(self):
 		""" Tests that the API can return codes and facts about Barack Obama"""
-		url = api_url + "/subject"
+		global username, token
+		print("\n\n")
+		url = api_url + "/api/subject"
 		topic = 'Barack Obama'
 		payload = {"topic":topic}
-		response = requests.get(url, params=payload)
+		response = requests.get(url, params=payload, auth=(username, token))
 		self.assertEqual(200, response.status_code)
 		json_response = response.json()
 		return_subject = json_response["subject"]
 		self.assertEqual(topic, return_subject)
 		fact = json_response["facts"]
 		self.assertTrue('Obama' in fact)
+		print("\n\n")
+		sleep(3)
 
 	@unittest.skipIf(api_url is "", "This test is disable until a URL has been defined for the API")
 	def test_get_fact_subject_fail(self):
 		""" Tests that an error message is returned when an invalid subject is given"""
-		url = api_url + "/subject"
+		global username, token
+		print("\n\n")
+		url = api_url + "/api/subject"
 		topic = 'ergopoiewgporewom'
 		payload = {"topic":topic}
-		response = requests.get(url, params=payload)
+		response = requests.get(url, params=payload, auth=(username, token))
 		self.assertEqual(404, response.status_code)		
 		json_response = response.json()
 		return_output = json_response["error"]
 		self.assertTrue(topic in return_output)
+		print("\n\n")
+		sleep(3)
 
 	@unittest.skipIf(api_url is "", "This test is disable until a URL has been defined for the API")
 	def test_get_fact_random(self):
 		""" Tests that a message is returned with a random subject"""
-		url = api_url + "/random"
-		response1 = requests.get(url)
+		global username, token
+		print("\n\n")
+		url = api_url + "/api/random"
+		response1 = requests.get(url, auth=(username, token))
 		json_response1 = response1.json()
 		subject1 = json_response1["subject"]
 
-		response2 = requests.get(url)
+		response2 = requests.get(url, auth=(username, token))
 		json_response2 = response2.json()
 		subject2 = json_response2["subject"]
 
 		print("\n\n", subject1, "  -----  ", subject2, "\n")
 		self.assertFalse(subject1 == subject2)
+		print("\n\n")
+		sleep(3)
 
 
 if __name__ == '__main__':
+	str_time = str(time.time())
+	username = "user" + str_time[:-8] + str_time[-7:]
+	password = "unittest"
+	response = requests.post(api_url + "/accounts", auth=(username, password))
+	if response.status_code != 200:
+		sys.exit()
+	json_response = response.json()
+	try:
+		token = json_response["Token"]
+	except KeyError:
+		sys.exit()
+
 	unittest.main()
